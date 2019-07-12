7e49533c473ffb90e6b2c02e72ae1d95a3695799
Ben Simms <ben@bensimms.moe>
2019-07-12 16:57:06 +0100

    Manually encode brightnesses

--- generate_brightnesses.py
+++ generate_brightnesses.py
@@ -1,13 +1,10 @@
 def generate():
     res = []
-    for i in range(16, -1, -1):
-        s = "1" + "0" * i
-        s *= 33 // (i + 1)
-        s = s.ljust(32, "0")
-        s = s[:32]
+    for i in range(32):
+        s = "0" * (32 - i) + "1" * i
         res.append(int(s, base=2))
     return res
 
 brightnesses = generate()
 for brightness in brightnesses:
-    print(hex(brightness))
+    print(f"0b{brightness:032b}")
